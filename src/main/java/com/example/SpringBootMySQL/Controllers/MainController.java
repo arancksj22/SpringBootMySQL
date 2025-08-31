29097d2e0f578993ac954ea94d97bc23addf4e66
arancksj22 <arancksj@gmail.com>
2025-08-31 19:08:35 +0530

    Service test

--- src/main/java/com/example/SpringBootMySQL/Controllers/MainController.java
+++ src/main/java/com/example/SpringBootMySQL/Controllers/MainController.java
@@ -1,13 +1,20 @@
 package com.example.SpringBootMySQL.Controllers;
 
+import com.example.SpringBootMySQL.Services.MainService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+    private final MainService mainService;
+
+    MainController(MainService mainService){
+        this.mainService = mainService;
+    }
+
     @GetMapping("/api")
     public String Get(){
-        return "tested";
+        return mainService.greet();
     }
 }
---
+++ src/main/java/com/example/SpringBootMySQL/Services/MainService.java
@@ -0,0 +1,10 @@
+package com.example.SpringBootMySQL.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MainService {
+    public String greet(){
+        return "Hello";
+    }
+}
